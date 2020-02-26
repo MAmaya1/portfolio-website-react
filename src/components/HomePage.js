f85fd47395e3ee380d8077c405bea016257ae2db
Mario Amaya <thefozcat@gmail.com>
2020-02-26 11:30:30 -0800

    fixed HomePage link

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = props => {
             <h2>React Developer</h2>
             <div className="web-icons">
                 <a href="https://www.linkedin.com/in/marioamayaweb" target="_blank"><i className="fab fa-linkedin fa-2x" /></a>
-                <a href="https://www.github.com/mamaya1"><i className="fab fa-github fa-2x" /></a>
+                <a href="https://www.github.com/mamaya1" target="_blank"><i className="fab fa-github fa-2x" /></a>
                 <a><i class="far fa-envelope fa-2x" /></a>
             </div>
         </div>
