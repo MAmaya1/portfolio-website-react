3c62f275ba3ca645a507ae2ffe86f55487633935
Mario Amaya <thefozcat@gmail.com>
2020-02-26 09:56:50 -0800

    configured router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi There!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hi There!</h1>
+      </div>
+    </Router>
   );
 }
 
