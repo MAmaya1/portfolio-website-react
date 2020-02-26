341b68947e3593127d3029726f3e09df4f4f0065
Mario Amaya <thefozcat@gmail.com>
2020-02-26 11:27:00 -0800

    fixed portrait

--- src/components/AboutMePage.js
+++ src/components/AboutMePage.js
@@ -10,7 +10,7 @@ const AboutMePage = props => {
             <h1>About <span>Me</span></h1>
             <h2>Let me tell you a few things...</h2>
             <div className="about-info">
-                <image src={portrait}/>
+                <img src={portrait}/>
                 <div className="bio">
                     <h3>BIO</h3>
                     <p>I am a React.js developer based in San Diego, and I have several years of experience as a highly-qualified project manager and customer service professional in the fields of multimedia and technology.</p>
