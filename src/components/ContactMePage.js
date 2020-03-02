66273fc6ce0cc678278b7a27eca15cfaad9342fd
Mario Amaya <thefozcat@gmail.com>
2020-03-02 09:06:10 -0800

    updated email links

--- src/components/ContactMePage.js
+++ src/components/ContactMePage.js
@@ -71,7 +71,7 @@ const ContactMePage = props => {
                 <LargeHeading>Contact <span>Me</span></LargeHeading>
                 <SmallHeading>This is how you can reach me...</SmallHeading>
                 <ContactIcons>
-                    <li><i className="far icon-bottom fa-envelope"/> <a>mario.amaya.web<wbr/>@gmail.com</a></li>
+                    <li><i className="far icon-bottom fa-envelope"/> <a href="mailto:mario.amaya.web@gmail.com">mario.amaya.web<wbr/>@gmail.com</a></li>
                     <li><i className="fas icon-bottom fa-phone"/> (619) 502-0073</li>
                     <li><i className="fab icon-bottom fa-linkedin"/> <a href="https://www.linkedin.com/in/marioamayaweb" target="_blank" rel="noopener noreferrer">marioamayaweb</a></li>
                     <li><i className="fab icon-bottom fa-github"/> <a href="https://github.com/MAmaya1" target="_blank" rel="noopener noreferrer">MAmaya1</a></li>
